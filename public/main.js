76c1a3b3569322a86f5006548b1b1fef8f627f6e
Atokigedesu <refuse.reality@gmail.com>
2016-08-07 23:00:42 +0900

    use hitbox-slider

--- public/main.js
+++ public/main.js
@@ -1,12 +1,10 @@
-const player = plyr.setup()[0];
-
 // スペースキーで 再生 or 一時停止 を切り替える
 document.addEventListener('keypress', event => {
   if (event.key !== ' ') return;
-  player.togglePlay();
+  document.querySelector('.plyr').plyr.togglePlay();
 });
 
 // ダブルクリックでフルスクリーンを切り替える
-document.addEventListener('dblclick', event => {
-  player.toggleFullscreen();
+document.addEventListener('dblclick', () => {
+  document.querySelector('.plyr').plyr.toggleFullscreen();
 });
