1ecf83fe3af5ee8311d7e80ea769ec40a9cb0401
Atokigedesu <refuse.reality@gmail.com>
2017-08-03 09:27:22 +0900

    disable cache

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const { execFile } = require('child_process');
 const express = require('express');
-const fs = require('fs');
 
 const app = express();
 app.set('view engine', 'jade');
@@ -9,28 +7,6 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   const srcUrl = req.query.src;
   res.render('index', { srcUrl });
-});
-
-app.get('/cache', (req, res) => {
-  const srcUrl = req.query.src;
-  const encodedUrl = encodeURIComponent(srcUrl);
-  const cacheRoot = `${__dirname}/public/cache`;
-  if (!/^https?:\/\//.test(srcUrl)) {
-    res.sendStatus(404);
-    return;
-  }
-  if (fs.existsSync(`${cacheRoot}/${encodedUrl}`)) {
-    res.sendStatus(200);
-  } else {
-    if (!fs.existsSync(`${cacheRoot}/.${encodedUrl}`)) {
-      execFile(`${__dirname}/cache.sh`, [srcUrl, encodedUrl], (err, stdout, stderr) => {
-        if (err) { throw err; }
-        console.log(stdout);
-        console.error(stderr);
-      });
-    }
-    res.sendStatus(202);
-  }
 });
 
 app.listen(8888);
--- public/main.js
+++ public/main.js
@@ -8,15 +8,3 @@ document.addEventListener('keypress', event => {
 document.addEventListener('dblclick', () => {
   document.querySelector('.plyr').plyr.toggleFullscreen();
 });
-
-// キャッシュ済みファイルがあれば、そちらに乗り換える
-const encodedUrl = encodeURIComponent(window.VideoTaggenSrcUrl);
-fetch(`/cache?src=${encodedUrl}`)
-  .then(res => {
-    if (res.status === 200) {
-      const doubleEncodedUrl = encodeURIComponent(encodedUrl);
-      const cachedSrc = encodeURIComponent(`/cache/${doubleEncodedUrl}`);
-      location.href = `/?src=${cachedSrc}`;
-    }
-  })
-  ;
