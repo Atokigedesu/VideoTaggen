1200b5a2d2cc92e956b812ac8e26e794e25506ea
Atokigedesu <refuse.reality@gmail.com>
2017-08-31 23:16:33 +0900

    replace hitbox-slide with plyr native

--- public/main.js
+++ public/main.js
@@ -1,10 +1,30 @@
-// スペースキーで 再生 or 一時停止 を切り替える
-document.addEventListener('keypress', event => {
-  if (event.key !== ' ') return;
-  document.querySelector('.plyr').plyr.togglePlay();
+const elem = document.querySelector('.player');
+const player = plyr.setup(elem, {
+  autoplay: true,
+  controls: [
+    "rewind",
+    "play",
+    "fast-forward",
+    "progress",
+    "current-time",
+    "mute",
+    "volume",
+    "fullscreen"
+  ],
+  seekTime: 30
+})[0];
+
+const mc = new Hammer.Manager(elem, {
+  recognizers: [
+    [Hammer.Tap],
+    [Hammer.Swipe, { direction: Hammer.DIRECTION_HORIZONTAL }]
+  ]
 });
 
-// ダブルクリックでフルスクリーンを切り替える
-document.addEventListener('dblclick', () => {
-  document.querySelector('.plyr').plyr.toggleFullscreen();
+mc.on('tap', (event) => {
+  if (event.tapCount !== 2) return;
+  player.toggleFullscreen();
 });
+
+mc.on('swipeleft' , () => player.rewind(10));
+mc.on('swiperight', () => player.forward(10));
